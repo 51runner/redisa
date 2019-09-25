79da452a6da4527b03daefbd5ac2df718b9c9952
runner <run@example.com>
2019-09-25 11:39:40 +0800

    使用django - redis

--- app01/views.py
+++ app01/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, HttpResponse
+from django_redis import get_redis_connection
+
 
 # Create your views here.
-import redis
-from utils.redis_poll import POOL
+# import redis
+# from utils.redis_poll import POOL
 
 
 def index(request):
-    conn = redis.Redis(connection_pool=POOL)
+    # conn = redis.Redis(connection_pool=POOL)
+    conn = get_redis_connection('default')
 
-    conn.hset('kkk', 6666)
+    conn.hset('kkk', 'age',6666)
     return HttpResponse('设置ok')
 
 
 def order(request):
-    conn = redis.Redis(connection_pool=POOL)
+    # conn = redis.Redis(connection_pool=POOL)
+    conn = get_redis_connection('default')
 
-    conn.hget('kkk')
+    conn.hget('kkk','age')
     return HttpResponse('获取ok')
